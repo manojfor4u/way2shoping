bea2189159d1b5e8c3e331b386ff1c19baddcb7f
Manoj Yadav <manojfor4u@gmail.com>
2021-01-11 21:00:47 +0530

    code updated

--- src/app/dashboard/account/account.component.ts
+++ src/app/dashboard/account/account.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
+  loginForm;
+  regForm;
+  indicator;
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = document.getElementById("LoginForm");
+    this.regForm = document.getElementById("RegForm");
+    this.indicator = document.getElementById("Indicator");
   }
 
+
+  register() {
+    this.regForm.style.transform = "translateX(0px)";
+    this.loginForm.style.transform = "translateX(0px)";
+    this.indicator.style.transform = "translateX(100px)";
+  }
+
+  login() {
+    this.regForm.style.transform = "translateX(300px)";
+    this.loginForm.style.transform = "translateX(300px)";
+    this.indicator.style.transform = "translateX(0px)";
+  }
+ 
 }
--- src/app/shared/layout-component/header/header.component.ts
+++ src/app/shared/layout-component/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  menuItems;
   constructor() { }
 
   ngOnInit(): void {
+    this.menuItems = document.getElementById("MenuItems");
+    this.menuItems.style.maxHeight = "0px";
   }
 
+  menutoggle() {
+    if (this.menuItems.style.maxHeight == "0px") {
+      this.menuItems.style.maxHeight = "200px";
+    }
+    else {
+      this.menuItems.style.maxHeight = "0px";
+    }
+  }
 }
